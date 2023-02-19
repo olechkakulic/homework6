4204dcb27ec91e1f36e87a5ac3f0b6273f6e9420
olechkakulic <olga.loginova@metalab.ifmo.ru>
2023-02-19 19:23:21 +0300

    did all tasks

---
+++ task41/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int GetPositiveElementsOfArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+int[] GetArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите элемент массива с индексом {i}: ");
+        arr[i] = int.Parse(Console.ReadLine());
+    }
+    return arr;
+}
+
+int PromptNumber(String text)
+{
+    Console.WriteLine(text);
+    return int.Parse(Console.ReadLine());
+}
+
+int size = PromptNumber("Введите количество элементов массив: ");
+int[] array = GetArray(size);
+int positiveNumber = GetPositiveElementsOfArray(array);
+Console.WriteLine($"{String.Join(" ", array)} -> {positiveNumber}");
+
+
---
+++ task43/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double intersectionsOfLinesInY(double[] array, double ValueX)
+{
+    double Y = (array[1] * ValueX + array[0]);
+    return Y;
+}
+double intersectionsOfLinesInX(double[] array)
+{
+    double X = ((array[2] - array[0]) / (array[1] - array[3]));
+    return X;
+}
+
+double[] GetArray(int size)
+{
+    double[] arr = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        if (i == 0)
+        {
+            Console.Write("Введите значение b1 (длина отрезка по оси OY, на который происходит сдвиг от начала координат): ");
+            arr[i] = Convert.ToDouble(Console.ReadLine());
+        }
+        else if (i == 1)
+        {
+            Console.Write("Введите значение k1 (угол наклона прямой): ");
+            arr[i] = Convert.ToDouble(Console.ReadLine());
+        }
+        else if (i == 2)
+        {
+            Console.Write("Введите значение b2: ");
+            arr[i] = Convert.ToDouble(Console.ReadLine());
+        }
+        else if (i == 3)
+        {
+            Console.Write("Введите значение k2: ");
+            arr[i] = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+    return arr;
+}
+
+double[] array = GetArray(4);
+double valueX = intersectionsOfLinesInX(array);
+double valueY = intersectionsOfLinesInY(array, valueX);
+Console.WriteLine($"b1 = {array[0]}, k1 = {array[1]}, b2 = {array[2]}, k2 = {array[3]} -> ({valueX}; {valueY})");
+
+
